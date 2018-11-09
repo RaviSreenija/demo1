5c384de303771a6d9d6f8e38f96b7aa191166a98
RaviSreenija <sreenija.ravi@softvision.com>
2018-11-09 12:34:22 +0530

    BTAT Assesment commit

---
+++ src/test/java/demo1/TestSample.java
@@ -0,0 +1,14 @@
+package demo1;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestSample {
+
+	@Test
+	public void test() {
+		//fail("Not yet implemented");
+	}
+
+}
